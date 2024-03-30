72dff3ddc9fa298b316997aad7d87898e3acaee3
Aman Gupta <amangupta.95@outlook.com>
2024-03-30 22:44:14 +0530

    CRUD API integrated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,13 +22,43 @@ function App() {
     });
   };
 
+  const addNewCategory = (name, parentId) => {
+    setIsLoading(true);
+    axios.post(apiUrl, { name, parentId }).then(() => {
+      getAllCategories();
+    });
+  };
+
+  const updateCategory = (id, name) => {
+    setIsLoading(true);
+    axios.patch(`${apiUrl}/${id}`, { name }).then(() => {
+      getAllCategories();
+    });
+  };
+
+  const deleteCategory = (id) => {
+    setIsLoading(true);
+    axios.delete(`${apiUrl}/${id}`).then(() => {
+      getAllCategories();
+    });
+  };
+
   return (
     <div
       className={`flex justify-center items-center ${
         isLoading ? 'h-[100vh]' : 'mt-4'
       }`}
     >
-      {isLoading ? <Loader /> : <TreeView data={categories} />}
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <TreeView
+          data={categories}
+          add={addNewCategory}
+          update={updateCategory}
+          remove={deleteCategory}
+        />
+      )}
     </div>
   );
 }
--- frontend/src/components/TreeView.jsx
+++ frontend/src/components/TreeView.jsx
@@ -3,26 +3,37 @@ import {
   faArrowDown,
   faCheck,
   faPen,
+  faPlus,
   faTrash,
   faXmark,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 
-function Node({ data }) {
+function Node({ data, add, update, remove }) {
   const [isOpen, setIsOpen] = useState(false);
   const [isEditable, setIsEditable] = useState(false);
-  const [catName, setCatName] = useState(data.name);
+  const [addNew, setAddNew] = useState(false);
+  const [newCat, setNewCat] = useState('');
+  const [updatedName, setUpdatedName] = useState(data.name);
 
   const toggleOpen = () => {
     setIsOpen((prev) => !prev);
   };
 
+  const addNewCategory = () => {
+    add(newCat, data.parent ? data.id : undefined);
+    setAddNew(false);
+  };
+
   const updateName = () => {
+    update(data.id, updatedName);
     setIsEditable(false);
   };
 
-  const handleDelete = () => {};
+  const handleDelete = () => {
+    remove(data.id);
+  };
 
   return (
     <div className="pl-3 mt-2">
@@ -41,8 +52,8 @@ function Node({ data }) {
             <input
               className="pl-1"
               type="text"
-              onChange={(e) => setCatName(e.target.value)}
-              value={catName}
+              onChange={(e) => setUpdatedName(e.target.value)}
+              value={updatedName}
             />
           ) : (
             <p className={`text-xl ${data.children.length ? '' : 'pl-4'}`}>
@@ -76,16 +87,50 @@ function Node({ data }) {
           />
         </div>
       </div>
+      {addNew ? (
+        <>
+          <input
+            className="pl-1"
+            type="text"
+            onChange={(e) => setNewCat(e.target.value)}
+            value={newCat}
+          />
+          <FontAwesomeIcon
+            icon={faCheck}
+            className="cursor-pointer scale-90 mx-1"
+            onClick={addNewCategory}
+          />
+          <FontAwesomeIcon
+            icon={faXmark}
+            className="cursor-pointer scale-90 mr-1"
+            onClick={() => setAddNew(false)}
+          />
+        </>
+      ) : (
+        <div className="flex justify-center mt-2">
+          <FontAwesomeIcon
+            icon={faPlus}
+            className="cursor-pointer"
+            onClick={() => setAddNew(true)}
+          />
+        </div>
+      )}
       {isOpen && <TreeView data={data.children} />}
     </div>
   );
 }
 
-function TreeView({ data }) {
+function TreeView({ data, add, update, remove }) {
   return (
     <div className="flex items-start flex-col">
       {data.map((category) => (
-        <Node key={category.id} data={category} />
+        <Node
+          key={category.id}
+          data={category}
+          add={add}
+          update={update}
+          remove={remove}
+        />
       ))}
     </div>
   );
